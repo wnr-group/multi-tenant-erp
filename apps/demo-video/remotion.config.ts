1ddd17ec34842f8fd543b9baa790f44293e42aab
dineshsy <s.ydinesh1177@gmail.com>
2026-05-10 19:14:55 +0530

    chore: remove demo-video from git tracking

--- apps/demo-video/remotion.config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from "@remotion/cli/config";
-
-Config.setEntryPoint("./src/index.ts");
-Config.setPublicDir("./public");
-Config.setConcurrency(8);
-Config.setVideoImageFormat("png");
--- apps/demo-video/src/easings.ts
+++
@@ -1,9 +0,0 @@
-import { Easing } from "remotion";
-
-export const easings = {
-  standard: Easing.bezier(0.4, 0, 0.2, 1),
-  soft: Easing.bezier(0.25, 0.46, 0.45, 0.94),
-  slowInLand: Easing.bezier(0.2, 0.9, 0.1, 1),
-  maskReveal: Easing.bezier(0.33, 1, 0.68, 1),
-  violentPush: Easing.bezier(0.7, 0, 0.1, 1),
-};
